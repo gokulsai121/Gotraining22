ad5d4a90dd7f16bc396ec1676cc2370bae8d3c89
gokulsai121 <90281110+gokulsai121@users.noreply.github.com>
2022-09-09 11:07:01 -0500

    Create evenodd.go

---
+++ evenodd.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a = []int{91, 42, 23, 14, 15, 76, 87, 28, 19, 95}
+	o := make(chan int)
+	e := make(chan int)
+
+	go odd(o)
+	go even(e)
+
+	for i := 0; i < len(a); i++ {
+		if a[i]%2 != 0 {
+			o <- a[i]
+		} else {
+			e <- a[i]
+		}
+	}
+
+}
+
+func odd(ch <-chan int) {
+	for v := range ch {
+		fmt.Println("odd:", v)
+	}
+}
+
+func even(ch <-chan int) {
+	for v := range ch {
+		fmt.Println("even:", v)
+	}
+}
